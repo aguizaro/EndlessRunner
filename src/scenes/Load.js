e46c81ee04ee03a3a2d1354133f19cec5028b148
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-05-10 00:14:01 -0700

    added texture atlas for sprite animation

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -2,4 +2,30 @@ class Load extends Phaser.Scene {
     constructor() {
         super('loadScene');
     }
+
+    preload() {
+        // loading bar
+        // see: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/loader/
+        let loadingBar = this.add.graphics();
+        this.load.on('progress', (value) => {
+            loadingBar.clear();                                 // reset fill/line style
+            loadingBar.fillStyle(0xFFFFFF, 1);                  // (color, alpha)
+            loadingBar.fillRect(0, centerY, w * value, 5);  // (x, y, w, h)
+        });
+        this.load.on('complete', () => {
+            loadingBar.destroy();
+        });
+
+        // load graphics assets
+        this.load.path = "./assets/";
+        this.load.atlas("ninja","img/ninja.png","img/ninja.json");
+        // load font
+        this.load.bitmapFont('shortStack', 'font/shortStack.png', 'font/shortStack.xml');
+        
+    }
+
+    create() {
+        // go to Title scene
+        this.scene.start('titleScene');
+    }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,4 +2,38 @@ class Play extends Phaser.Scene {
     constructor() {
         super('playScene');
     }
+
+    create(){
+        this.anims.create({
+             key: 'ninja_run',
+             frames: this.anims.generateFrameNames('ninja', { 
+                prefix: 'run_',
+                suffix: '.png',
+                end: 5, 
+            }), repeat: -1 });
+
+        this.anims.create({
+             key: 'ninja_jump',
+             frames: this.anims.generateFrameNames('ninja', { 
+                prefix: 'run_',
+                suffix: '.png',
+                end: 3,
+            }), repeat: -1 });
+
+        this.ninja= this.physics.add.sprite(100,500, "ninja").play('ninja_run');
+        // set up cursor keys
+        cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update(time, delta){
+        if (cursors.space.isDown){
+             this.ninja.play('ninja_jump');
+            this.ninja.setVelocityY(-speed);
+        }else{
+            this.ninja.setY(500);
+        }
+
+
+
+    }
 }
